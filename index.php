6dcf83a5447cb0594009f1eaad1fe15cb23948f3
Chatchanon63070033 <73678877+Chatchanon63070033@users.noreply.github.com>
2020-10-30 10:45:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
-<?php
-echo "<h2>ITF So cute!</h2>";
-echo "Hello World!<br>";
-echo "Chatchanon kaewsukkho Mark<br>";
-<img src=
-?>
+<html>
+
+<head>
+    <title>Hello World</title>
+</head>
+
+<body>
+    <h1>Sawasdeekrub</h1>
+    <p>Cat in the sky</p>
+    <img src="cat.jpg">
+    <br>
+    <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+
+    <h2>Good Morning</h2>
+    <p>Opz TV</p>
+    <img src="opz.jpg">
+    <br>
+    <a href="http://wwww.it.kmitl.ac.th">This is a link</a>
+
+    <h3>Good night</h3>
+    <p>bedroom</p>
+    <img src="bedroom.jpg">
+    <br>
+    <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+
+    <h4>Thank You</h4>
+    <ul>
+        <li>Google</li>
+        <li>Yahoo</li>
+        <li>Amazon</li>
+    </ul>
+
+</body>
+
+</html>
